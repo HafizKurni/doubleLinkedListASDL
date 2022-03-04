d5a2c5558a0fe4baea66c04b3ebfe7b272457184
Hafiz M Kurniawan <87954942+HafizKurni@users.noreply.github.com>
2022-03-04 15:57:14 +0700

    Intro

--- MainDoubleLinkedList.java
+++ MainDoubleLinkedList.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 
 public class MainDoubleLinkedList{
+     static LinkedList<Integer> lListP = new LinkedList<>();
+     
 
      public static void main(String[] args){
-         Double2LinkedList lList = new Double2LinkedList();
-
+         
          Scanner input = new Scanner(System.in);
+          
+         System.out.println("[--------Pilih Cara Pengerjaan----------]");
+	    System.out.println("1. Secara Manual ");
+	    System.out.println("2. Secara Pustaka ");
+	    System.out.print("Silakan pilih antara [1/2] : ");
          int pilih = input.nextInt();
 
          if(pilih == 1){
+              Double2LinkedList lList = new Double2LinkedList();
              lList.manual();
          }else{
              lList.pustaka(); // Belum ada function nya
